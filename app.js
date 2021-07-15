d6fff6fc5e5a79b08f23de5bc8ad37605a01bec4
Bethel Berhanemariam <bethelberhanemariam@Bethels-MBP.fios-router.home>
2021-07-15 00:14:25 -0400

    completed RESTful routes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override')
 const Campground = require('./models/campground');
 const app = express();
 
@@ -13,13 +14,55 @@ console.log('mongoose connected');
     console.log(err)
 });
 
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
 app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({});
-    res.render('index', { campgrounds });
+    res.render('campgrounds/index', { campgrounds });
 })
+
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new')
+})
+
+app.get('/campgrounds/:id', async (req, res) => {
+    const {id} = req.params;
+    const campground = await Campground.findById(id);
+    res.render('campgrounds/show', {campground});
+})
+
+app.post('/campgrounds', async (req, res) => {
+    const { campground } = req.body;
+    const newCampground = new Campground({...campground});
+    await newCampground.save();
+    res.redirect(`/campgrounds/${newCampground._id}`);
+})
+
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id);
+    res.render('campgrounds/edit', {campground});
+})
+
+app.patch('/campgrounds/:id', async (req, res) => {
+    const {id} = req.params;
+    const {campground} = req.body;
+    const updatedCampground = await Campground.findByIdAndUpdate(id, {...campground}, {new: true});
+    res.redirect(`/campgrounds/${updatedCampground._id}`);
+})
+
+app.delete('/campgrounds/:id', async (req, res) => {
+    const {id} = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
+})
+
+
+
 
 
 app.listen(3000, () => {
